b10d26f01ab03e4e0e259eb3aeb6181cc4cca863
Kaue Pereira <kauefontes@outlook.com>
2018-10-01 22:33:50 -0400

    Initial Commit

---
+++ MyFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by Kaue Pereira on 01/10/18.
+//  Copyright © 2018 Kaue Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by Kaue Pereira on 01/10/18.
+//  Copyright © 2018 Kaue Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
